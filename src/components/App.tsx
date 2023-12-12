99e819dc1dfccc8a70ba7c1a430ab8fa82b957b0
Olya <katniss30051991@gmail.com>
2023-12-12 18:11:26 +0200

    add: routes files

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
 import Home from "../pages/Home";
-// import { Loader } from "./Loader/Loader";
+import { Loader } from "./Loader/Loader";
 
 function App() {
   return (
     <>
       <Home />
-      {/* <Loader /> */}
+      <Loader />
     </>
   );
 }
---
+++ src/components/PrivateRoute.ts
@@ -0,0 +1,9 @@
+// import { Navigate } from "react-router-dom";
+// import { useAuth } from "../hooks/useAuth";
+
+// export const PrivateRoute = ({ component: Component, redirectTo = "/" }) => {
+//   const { isLoggedIn, isRefreshing } = useAuth();
+
+//   const shoudRedirect = !isLoggedIn && !isRefreshing;
+//   return shoudRedirect ? <Navigate to={redirectTo} /> : Component;
+// };
